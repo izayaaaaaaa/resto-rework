06021419270337b7c1c39ba7ca5e1ad3719497b8
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-12-26 08:25:51 +0800

    login layout (except the toggle visibility icon)

--- restaurant-menu-system/src/main/java/restaurant/LoginForm.java
+++ restaurant-menu-system/src/main/java/restaurant/LoginForm.java
@@ -1,41 +1,135 @@
 package restaurant;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class LoginForm extends JFrame {
     private JTextField usernameField;
     private JPasswordField passwordField;
+    private JButton viewPasswordButton;
+    private ImageIcon eyeOpenIcon = new ImageIcon("src/main/java/photos/1eye.png");
+    private ImageIcon eyeClosedIcon = new ImageIcon("src/main/java/photos/2eye.png");
 
     public LoginForm() {
+        getContentPane().setBackground(Color.WHITE); // Set the background to white
+        setUndecorated(true);
         createUI();
+        Border orangeBorder = BorderFactory.createLineBorder(new Color(255, 102, 0)); // Create the border
+        ((JComponent) getContentPane()).setBorder(orangeBorder); // Set the border to the content pane
     }
 
     private void createUI() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
-        add(new JLabel("Username:"));
-        usernameField = new JTextField();
-        add(usernameField);
+        // Title
+        JLabel titleLabel = new JLabel("Restaurant Menu");
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 48));
+        titleLabel.setForeground(Color.BLACK);
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(20, 10, 20, 10);
+        add(titleLabel, gbc);
 
-        add(new JLabel("Password:"));
-        passwordField = new JPasswordField();
-        add(passwordField);
+        // Username Label and Field
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        add(usernameLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        usernameField = new JTextField(20);
+        usernameField.setFont(new Font("Arial", Font.PLAIN, 14));
+        usernameField.setBorder(BorderFactory.createCompoundBorder(
+                usernameField.getBorder(),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5))); // Add padding
+        add(usernameField, gbc);
+
+        // Invisible label for alignment
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        add(new JLabel(""), gbc);
+
+        // Password Label and Field
+        gbc.gridwidth = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordLabel.setFont(new Font("Arial", Font.PLAIN, 24));
+        add(passwordLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        passwordField = new JPasswordField(20);
+        passwordField.setFont(new Font("Arial", Font.PLAIN, 14));
+        passwordField.setBorder(BorderFactory.createCompoundBorder(
+                passwordField.getBorder(),
+                BorderFactory.createEmptyBorder(5, 5, 5, 5))); // Add padding
+        add(passwordField, gbc);
+
+        // View Password Button
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        viewPasswordButton = new JButton(eyeClosedIcon);
+        styleIconButton(viewPasswordButton);
+        viewPasswordButton.setBorderPainted(false);
+        viewPasswordButton.setContentAreaFilled(false);
+        viewPasswordButton.setFocusPainted(false);
+        viewPasswordButton.setOpaque(false);
+        viewPasswordButton.addActionListener(e -> togglePasswordVisibility());
+        add(viewPasswordButton, gbc);
+
+        // Button Panel with FlowLayout
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0)); // 10-pixel gap between components
+        buttonPanel.setOpaque(false); // Make the panel transparent
 
         JButton loginButton = new JButton("Login");
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                authenticate(usernameField.getText(), new String(passwordField.getPassword()));
-            }
-        });
-        add(loginButton);
+        styleButton(loginButton, new Color(255, 102, 0), Color.WHITE);
+        loginButton.setFont(new Font("Arial", Font.BOLD, 18));
+        loginButton
+                .addActionListener(e -> authenticate(usernameField.getText(), new String(passwordField.getPassword())));
+        buttonPanel.add(loginButton);
 
-        setSize(300, 150);
+        gbc.gridx = 2;
+        gbc.gridy = 3;
+        gbc.insets = new Insets(10, 5, 10, 10);
+        JButton cancelButton = new JButton("Cancel");
+        styleButton(cancelButton, new Color(232, 58, 15), Color.WHITE);
+        cancelButton.setFont(new Font("Arial", Font.BOLD, 18));
+        cancelButton.addActionListener(e -> System.exit(0));
+        buttonPanel.add(cancelButton);
+
+        // Add the button panel to the main layout
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        add(buttonPanel, gbc);
+
+        setSize(720, 390);
         setLocationRelativeTo(null);
+    }
+
+    private void togglePasswordVisibility() {
+        if (passwordField.getEchoChar() != '\0') {
+            passwordField.setEchoChar('\0');
+            viewPasswordButton.setIcon(eyeOpenIcon);
+        } else {
+            passwordField.setEchoChar('*');
+            viewPasswordButton.setIcon(eyeClosedIcon);
+        }
     }
 
     private void authenticate(String username, String password) {
@@ -52,6 +146,22 @@ public class LoginForm extends JFrame {
         } else {
             JOptionPane.showMessageDialog(this, "Invalid credentials", "Error", JOptionPane.ERROR_MESSAGE);
         }
+    }
+
+    private void styleIconButton(JButton button) {
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
+        button.setOpaque(false);
+        button.addActionListener(e -> togglePasswordVisibility());
+    }
+
+    private void styleButton(JButton button, Color backgroundColor, Color textColor) {
+        button.setBackground(backgroundColor);
+        button.setForeground(textColor);
+        button.setFont(new Font("Arial", Font.BOLD, 18));
+        button.setOpaque(true);
+        button.setBorderPainted(false);
     }
 
     public static void main(String[] args) {
